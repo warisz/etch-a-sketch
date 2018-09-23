a78e73587073bc34db56c6ba90419024dc07fc9a
Waris Z <wariszahoor0@gmail.com>
2018-09-22 21:21:44 -0400

    No functionality

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 const divContainer = document.getElementById("divContainer");
+const randomButton = document.querySelector('#randColor');
+const blackButton = document.querySelector('#black');
+const eraseButton = document.querySelector('#erase');
+
+let color = "black";
 createGrid(16);
 
+randomButton.addEventListener('click', randomClick);
+blackButton.addEventListener('click', blackClick);
+eraseButton.addEventListener('click', eraseClick);
+
+function randomClick(){
+  color = `rgb(${Math.random()*256},${Math.random()*256},${Math.random()*256})`;
+}
+
+function eraseClick(){
+  color = "white";
+}
+
+function blackClick(){
+  color = "black";
+}
 
 function createGrid(boxes){
     for(i=0; i<boxes*boxes; i++){
         console.log(i);
         const div = document.createElement('div');
+        div.setAttribute=('id', 'box');
         div.style.width = divContainer.clientWidth/boxes + 'px';
         div.style.height = divContainer.clientHeight/boxes + 'px';
         div.style.boxShadow = '0px 0px 0px 1px black inset';
@@ -13,9 +34,12 @@ function createGrid(boxes){
         div.style.padding = '0px';
         div.style.display = 'inline-block';
         div.style.float = 'left';
-        div.onmouseover= function(){
-          div.style.backgroundColor = 'black';
-        }
         divContainer.appendChild(div);
   }
 }
+
+$("#box").hover(function(){
+  $(this).css("background-color", color);
+    }, function(){
+    $(this).css("background-color", color);
+});
